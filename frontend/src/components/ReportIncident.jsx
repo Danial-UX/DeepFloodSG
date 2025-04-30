e7d5db1cea51f6a2ce00280c20d2696ba7b5eee7
DANIAL <danialricciardo99@gmail.com>
2025-04-30 18:45:47 +0800

    added report incident page

---
+++ frontend/src/components/ReportIncident.jsx
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Typography,
+  Paper,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  Button,
+  FormControl,
+  FormLabel,
+  TextField,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+const ReportIncident = () => {
+  const navigate = useNavigate();
+  const [responses, setResponses] = useState({
+    location: "",
+    seenFlood: "",
+    waterLevel: "",
+    damageExtent: "",
+    roadBlocked: "",
+  });
+
+  const handleChange = (event) => {
+    setResponses({
+      ...responses,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = () => {
+    console.log("Submitted Responses:", responses);
+    alert("Thank you for your feedback!");
+    navigate("/");
+  };
+
+  return (
+    <Paper
+      elevation={5}
+      sx={{
+        maxWidth: 600,
+        margin: "40px auto",
+        padding: 4,
+        borderRadius: 3,
+      }}
+    >
+      <Typography variant="h4" gutterBottom>
+        Flood Incident Report
+      </Typography>
+
+      <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
+        Please provide the location of the flood:
+      </Typography>
+      <TextField
+        fullWidth
+        label="Flood Location"
+        name="location"
+        value={responses.location}
+        onChange={handleChange}
+        sx={{ mb: 3 }}
+      />
+
+      <Box sx={{ mt: 3 }}>
+        <FormControl component="fieldset" sx={{ mb: 3 }}>
+          <FormLabel>1. Did you witness a flood today?</FormLabel>
+          <RadioGroup name="seenFlood" value={responses.seenFlood} onChange={handleChange}>
+            <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+            <FormControlLabel value="no" control={<Radio />} label="No" />
+          </RadioGroup>
+        </FormControl>
+
+        <FormControl component="fieldset" sx={{ mb: 3 }}>
+          <FormLabel>2. How deep was the water?</FormLabel>
+          <RadioGroup name="waterLevel" value={responses.waterLevel} onChange={handleChange}>
+            <FormControlLabel value="ankle" control={<Radio />} label="Ankle-deep" />
+            <FormControlLabel value="knee" control={<Radio />} label="Knee-deep" />
+            <FormControlLabel value="waist" control={<Radio />} label="Waist-deep or higher" />
+          </RadioGroup>
+        </FormControl>
+
+        <FormControl component="fieldset" sx={{ mb: 3 }}>
+          <FormLabel>3. Was there any property damage?</FormLabel>
+          <RadioGroup name="damageExtent" value={responses.damageExtent} onChange={handleChange}>
+            <FormControlLabel value="none" control={<Radio />} label="No damage" />
+            <FormControlLabel value="minor" control={<Radio />} label="Minor damage" />
+            <FormControlLabel value="severe" control={<Radio />} label="Severe damage" />
+          </RadioGroup>
+        </FormControl>
+
+        <FormControl component="fieldset" sx={{ mb: 3 }}>
+          <FormLabel>4. Were any roads or walkways blocked?</FormLabel>
+          <RadioGroup name="roadBlocked" value={responses.roadBlocked} onChange={handleChange}>
+            <FormControlLabel value="no" control={<Radio />} label="No" />
+            <FormControlLabel value="some" control={<Radio />} label="Some areas" />
+            <FormControlLabel value="yes" control={<Radio />} label="Yes, completely" />
+          </RadioGroup>
+        </FormControl>
+
+        <Button
+          variant="contained"
+          color="primary"
+          sx={{ mt: 2 }}
+          fullWidth
+          onClick={handleSubmit}
+        >
+          Submit Report
+        </Button>
+      </Box>
+    </Paper>
+  );
+};
+
+export default ReportIncident;
